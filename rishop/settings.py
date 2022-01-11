f4197431262a4a6f96c9e9096a7842bb02fe7a63
Danang Haris Setiawan <mr.danangharissetiawan@gmail.com>
2022-01-11 11:17:49 +0700

    change settings

--- rishop/settings.py
+++ rishop/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'rishop.herokuapp.com']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
