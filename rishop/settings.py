8af299091a501520d81eb3f85417efe34d118ff0
Danang Haris Setiawan <mr.danangharissetiawan@gmail.com>
2022-01-14 20:44:10 +0700

    fixed error

--- rishop/settings.py
+++ rishop/settings.py
@@ -85,8 +85,6 @@ DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 #         'NAME': env("DATABASE_NAME"),
-#         'USER': env("DATABASE_USER"),
-#         'PASSWORD': env("DATABASE_PASS"),
 #         'HOST': 'localhost',
 #         'PORT': '',
 #     }
